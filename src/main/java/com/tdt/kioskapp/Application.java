184eb30c2566d256c804c25c16c8f3f34ce6530e
khoi <le.khoi992@gmail.com>
2015-11-06 10:39:00 +0700

    refactor code

--- src/main/java/com/tdt/kioskapp/Application.java
+++ src/main/java/com/tdt/kioskapp/Application.java
@@ -2,17 +2,13 @@ package com.tdt.kioskapp;
 
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
-import com.tdt.kioskapp.config.AppConfig;
 import com.tdt.kioskapp.ui.KioskUI;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import uk.co.caprica.vlcj.binding.LibVlc;
